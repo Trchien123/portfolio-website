e29d0b0f83fefdd03eda8d2d6736e80a4f34c4ba
Trchien123 <huynhtrungchienltt@gmail.com>
2026-02-28 11:04:16 +0700

    update project image

--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -262,7 +262,7 @@ export const portfolioData = {
       description:
         "This project is being uploaded soon.",
       image:
-        "hhttps://images.unsplash.com/photo-1680969295989-43b229a4fce3?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1680969295989-43b229a4fce3?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       tech: ["TensorFlow", "Keras", "Deep Learning", "React"],
       link: "#",
     },
